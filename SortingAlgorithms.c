f72924ad91d943f36c029b5c0c7f5a79576c9abf
Randz <158979362+Randz0@users.noreply.github.com>
2025-03-14 13:56:01 -0400

    Update SortingAlgorithms.c

--- SortingAlgorithms.c
+++ SortingAlgorithms.c
@@ -4,7 +4,7 @@
 #define PI 3.1415926535f
 #define HALF_PI 1.5707963f
 
-// Macro booleans
+// Macros for booleans. Very convienient
 #define boolean unsigned char
 #define TRUE 1
 #define FALSE 0
@@ -29,33 +29,41 @@ float QuickSin(float value) {
     return TaylorApprox(value);
 }
 
+void PrintIntArray(int* toPrint, int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%d \n", *(toPrint + i));
+    }
+}
+
 // Returns a copy of an integer array from another
 int* CopyIntArray(int* copying, int length) {
-    int* copy = malloc(sizeof(int*) * length);
+    int* copy = malloc(sizeof(int) * length);
 
     for (int i = 0; i < length; i++) {
-        *(copy + i) = *(copying + i); // Copying the individual values
+        copy[i] = copying[i]; // Copying the individual values
     }
 
     return copy;
 }
 
+// Sorts the given array using a bubble sort algorithm
 int* BubbleSort(int* toSort, int length) {
     int* returnedCopy = CopyIntArray(toSort, length);
 
-    boolean isSorted = FALSE; // 1 = Sorted, 0 = Unsorted
+    boolean isSorted = FALSE;
+    int temp; // Used when actually swapping
 
     while (isSorted == FALSE) {
         isSorted = TRUE;
 
-        for (int i = 0; i < length - 1; i++) { // Only check for swaps up to the second to last element
-            if ( *(returnedCopy + i) <= *(returnedCopy + i + 1)) {
+        for (int i = length - 1; i >= 0; i--) { // Only check for swaps up to the second to last element since there's no +1 element to swap with the last one
+            if (returnedCopy[i] <= returnedCopy[i + 1]) {
                 continue; // Elements are sorted keep going
             }
 
-            int temp = *(returnedCopy + i); // storing the value that will be overwritten
-            *(returnedCopy + i) = *(returnedCopy + i + 1);
-            *(returnedCopy + i + 1) = temp; // Swapping the two values to put then in order
+            temp = returnedCopy[i]; // storing the value that will be overwritten
+            returnedCopy[i] = returnedCopy[i + 1];
+            returnedCopy[i + 1] = temp; // Swapping the two values to put then in order
 
             isSorted = FALSE;
         }
@@ -64,17 +72,54 @@ int* BubbleSort(int* toSort, int length) {
     return returnedCopy;
 }
 
-void PrintIntArray(int* toPrint, int length) {
-    for (int i = 0; i < length; i++) {
-        printf("%d \n", *(toPrint + i));
+// Handles the insertion part of the Insertion Sort Algorithm
+void InsertElement(int from, int where, int* inserting) {
+    if (from == where) {
+        return; // Nothing to insert
     }
+
+    int pushNext = inserting[where];
+    int temp;
+
+    for (int pushingIndex = where; pushingIndex < from; pushingIndex++) {
+        // Push the value into the next index
+        temp = inserting[pushingIndex + 1]; // Store next value that will be pushed
+        inserting[pushingIndex + 1] = pushNext; // Push the current value up
+
+        pushNext = temp; // Swap the current value for the next value to be pushed
+    }
+
+    inserting[where] = pushNext;
+}
+
+// Sorts the given array using an insertion sort algorithm
+int* InsertionSort(int* toSort, int length) {
+    int* returnedCopy = CopyIntArray(toSort, length);
+
+    for (int i = 1; i < length; i++) { // i denotes which element is being inserted
+        
+        for (int j = 0; j < i; j++) { // Scrolling through the sorted portion
+
+            if (returnedCopy[i] > returnedCopy[j]) {
+                continue;
+            }            
+
+            // found the insertion index
+            InsertElement(i, j, returnedCopy);
+
+            break;
+        }
+    }
+
+    return returnedCopy;
 }
 
 int main() {
-    int instance[4] = {4, 3, 2, 1};
+    int instance[7] = {10, 7, 6, 3, 5, 4, 22};
     int length = sizeof(instance) / sizeof(instance[0]);
 
-    int* sorted = BubbleSort(instance, length);
+    int* sorted = InsertionSort(instance, length);
+
     PrintIntArray(sorted, length);
 
     getchar();
